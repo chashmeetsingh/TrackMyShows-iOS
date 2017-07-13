063fad59d66393962093b53450bbc51bb5ff711f
Chashmeet Singh <chashmeetsingh@gmail.com>
2017-07-13 10:54:59 +0530

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '8.0'
+
+target 'TMS' do
+  use_frameworks!
+
+  # Pods for TMS
+  pod 'MaterialComponents'
+  pod 'Alamofire'
+end
---
+++ TMS/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  TMS
+//
+//  Created by Chashmeet Singh on 2017-07-13.
+//  Copyright © 2017 Chashmeet Singh. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        
+        
+        return true
+    }
+
+}
+
